184294ba9c21f64cd46937aed43657ddb9c7881d
MateuszSuchora <mateusz_suchora@wp.pl>
2021-12-28 21:43:36 +0100

    28.12.2021 - version v1. still have to add private repos list and some view to details

--- checkingGitStatus/urls.py
+++ checkingGitStatus/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-
-from .views import RepoListView, all_added_repos
+from checkingGitStatus.views import RepoListView, all_added_repos, add_repo, add_repo_from_request
 
 app_name = 'repos'
 
 urlpatterns = [
     path('', RepoListView.as_view(), name='repo_list'),
     path('getRepos/', all_added_repos),
+    path('repos/add/', add_repo),
+    path('repos/add/addRepo/', add_repo_from_request),
+
 ]
--- checkingGitStatus/views.py
+++ checkingGitStatus/views.py
@@ -1,8 +1,11 @@
 import json
 
+from allauth.socialaccount.models import SocialToken
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import ListView
+
 from .models import Repo, select_all_repos
 from rest_framework.decorators import api_view
 
@@ -16,8 +19,35 @@ class RepoListView(ListView):
 def all_added_repos(request):
     try:
         repos = select_all_repos()
-        results_to_template = {'repos_list': repos, 'count': len(repos)}
+        user = request.user
+        result = SocialToken.objects.filter(account__user=user)
+        results_to_template = {'repos_list': repos, 'count': len(repos), 'token': str(result[0])}
         return HttpResponse(json.dumps(results_to_template), content_type="application/json")
     except:
+        response_data = {'result': 'error'}
+        return HttpResponse(json.dumps(response_data), content_type="application/json")
+
+
+def add_repo(request):  # main/basic page
+    return render(request, 'add_repo.html')
+
+
+@csrf_exempt
+@api_view(['POST'])
+def add_repo_from_request(request):
+    try:
+        if request.method == "GET":
+            response_data = {'result': 'wrong request'}
+            return HttpResponse(json.dumps(response_data), content_type="application/json")
+        if request.method == "POST":
+            json_data = request.data
+            name = json_data["name"]
+            is_private = json_data["is_private"]
+            owner = json_data["owner"]
+            Repo.objects.create(name=name, is_private=is_private, owner=owner)
+            response_data = {'result': 'repo added'}
+            return HttpResponse(json.dumps(response_data), content_type="application/json")
+    except:
+        raise
         response_data = {'result': 'error'}
         return HttpResponse(json.dumps(response_data), content_type="application/json")
